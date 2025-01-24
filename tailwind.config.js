ae02f6572478f8ae06a6b5b26971ed4acb6649dc
NajlaTaghiour <NajlaTaghipour@gmail.com>
2025-01-24 21:17:43 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Vazirmatn"],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "class",
+    }),
+  ],
+};
